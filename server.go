f93e4a4f2b766d1bc7f8316a97ab6671484cee89
thuhoangle <thisishoangf@gmail.com>
2024-07-08 10:21:48 +0700

    add Dockerfile

---
+++ server.go
@@ -0,0 +1,99 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{
+	// Solve cross-domain problems
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+} // use default options
+
+func ws(c *gin.Context, msgCh chan string) {
+	// Upgrade get request to WebSocket protocol
+	ws, err := upgrader.Upgrade(c.Writer, c.Request, nil)
+	if err != nil {
+		log.Print("upgrade:", err)
+		return
+	}
+	defer ws.Close()
+
+	go func() {
+		for {
+			// Read data from ws
+			mt, message, err := ws.ReadMessage()
+			if err != nil {
+				log.Println("read:", err)
+				break
+			}
+			log.Printf("recv: %s", message)
+
+			// Process client message
+			switch string(message) {
+			case "Ping":
+				msgCh <- "pong"
+			default:
+				msgCh <- "Unknown command"
+			}
+
+			// Echo message back to client
+			err = ws.WriteMessage(mt, message)
+			if err != nil {
+				log.Println("write:", err)
+				break
+			}
+		}
+	}()
+
+	for {
+		select {
+		case msg := <-msgCh:
+			err := ws.WriteMessage(websocket.TextMessage, []byte(msg))
+			if err != nil {
+				log.Println("write:", err)
+				return
+			}
+		}
+	}
+}
+
+func main() {
+	fmt.Println("Websocket Server!")
+
+	bindAddress := "localhost:8448"
+	r := gin.Default()
+
+	// Create a channel for console messages
+	msgCh := make(chan string)
+
+	// Start a goroutine to read input from console
+	go func() {
+		reader := bufio.NewReader(os.Stdin)
+		for {
+			fmt.Print("Enter message: ")
+			text, err := reader.ReadString('\n')
+			if err != nil {
+				log.Println("Error reading from console:", err)
+				continue
+			}
+			text = strings.TrimSpace(text)
+			msgCh <- text
+		}
+	}()
+
+	r.GET("/ws", func(c *gin.Context) {
+		ws(c, msgCh)
+	})
+
+	r.Run(bindAddress)
+}
